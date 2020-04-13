ebb26d1763d95f03814db3404245afd3e890b3a1
harlen lopez <harlen.d.lopez@gmail.com>
2020-04-13 11:11:13 -0700

    added our hello world

---
+++ APIBackEnd/APIBackEnd/Controllers/Test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBackEnd.Controllers
+{
+    [Route("api/hello")]
+    [ApiController]
+
+    public class Test
+    {
+        [HttpGet]
+        public string Hello()
+        {
+            return "hello world";
+        }
+    }
+}
