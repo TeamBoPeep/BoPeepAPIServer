b6acd63319c43f189b30d52984c82ad54ed9b64a
harlen lopez <harlen.d.lopez@gmail.com>
2020-04-13 15:09:41 -0700

    creadted a migration and seeded new data for activities

---
+++ APIBackEnd/APIBackEnd/Models/DTO/ActivitiesDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBackEnd.Models.DTO
+{
+    public class ActivitiesDTO
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Rate Rate { get; set; }
+        public double Rating { get; set; }
+        public Location Location { get; set; }
+        public string ExternalLink { get; set; }
+
+        //naviggation properties
+        public List<Reviews> Reviews = new List<Reviews>();
+        public List<Tag> Tag = new List<Tag>();
+
+    }
+}
